9004fbf4473b8dc60fa9930d5aa575de89fd3b2e
csordas erik <csordaserik92@gmail.com>
2019-11-24 17:37:11 +0100

    elakadtás

---
+++ szamol.js
@@ -0,0 +1,12 @@
+function szamol(){
+var a = parseInt(document.getElementById("a").value);
+var b = parseInt(document.getElementById("b").value);
+var c = parseInt(document.getElementById("c").value);
+var d;
+
+d = b * b - 4 * a * c;
+d = math.sqrt(d);
+var eredMeny = document.getElementById("eredmeny");
+eredMeny.innerText ="ddsd";
+
+}
